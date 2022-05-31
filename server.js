766a8cb3bf5a89fa9ec3fec60b275f90ec32ca92
Fu11Stack <96635777+Fu11Stack@users.noreply.github.com>
2022-05-31 12:09:26 -0700

    Web Page Virtual Dom Path Update
    Server.js path update
    Config.env Update

--- server.js
+++ server.js
@@ -13,6 +13,15 @@ if(process.env.NODE_ENV === 'development') {
 
 app.use('/api/v1/profile', require('./routes/profile'));
 
+// Handle production
+if(proccess.env.NODE_ENV === `production`) {
+  // Set static folder
+  app.use(express.static(__dirname + `/public/`));
+
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + `/public/index.html`));
+}
+
+  //Handle SPA
 const port = process.env.PORT || 8000;
 
 
@@ -21,4 +30,4 @@ const port = process.env.PORT || 8000;
 app.listen(5000, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`);
 
-});
+})
