e57af8434fa205e3f0fedb3ed0dd44dbb398b20d
Fu11Stack <96635777+Fu11Stack@users.noreply.github.com>
2022-06-23 21:15:02 -0700

    Update server.js

--- server.js
+++ server.js
@@ -13,9 +13,6 @@ if(process.env.NODE_ENV === 'development') {
 
 app.use('/api/v1/profile', require('./routes/profile'));
 
-// Handle production
-if(proccess.env.NODE_ENV === `production`) {
-  // Set static folder
   app.use(express.static(__dirname + `/public/`));
 
   app.get(/.*/, (req, res) => res.sendFile(__dirname + `/public/index.html`));
